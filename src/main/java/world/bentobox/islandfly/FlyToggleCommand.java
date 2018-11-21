a4940614956ebf965ccbf0b0372f5d2b32399881
DarkRails <gui.freyermuth@gmail.com>
2018-11-21 14:51:57 +0100

    Small change in a condition

--- src/main/java/world/bentobox/islandfly/FlyToggleCommand.java
+++ src/main/java/world/bentobox/islandfly/FlyToggleCommand.java
@@ -28,7 +28,7 @@ public class FlyToggleCommand extends CompositeCommand {
         final IslandsManager islands = this.getIslands();
         islands.userIsOnIsland(user.getWorld(), user);
         final Optional<Island> island = islands.getIslandAt(user.getLocation());
-        if((!island.isPresent() || !island.get().getMembers().containsKey(user.getUniqueId())) && !user.hasPermission("islandfly.bypass")){
+        if(!user.hasPermission("islandfly.bypass") && (!island.isPresent() || !island.get().getMembers().containsKey(user.getUniqueId()))){
             user.sendMessage("islandfly.command.only-on-island");
             return true;
         }
