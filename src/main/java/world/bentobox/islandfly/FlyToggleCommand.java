b35e78f97379c8832863b7d3890c7b7558eba33b
DarkRails <gui.freyermuth@gmail.com>
2018-11-21 14:37:12 +0100

    Allow fly command only on island

--- src/main/java/world/bentobox/islandfly/FlyToggleCommand.java
+++ src/main/java/world/bentobox/islandfly/FlyToggleCommand.java
@@ -3,8 +3,11 @@ package world.bentobox.islandfly;
 import org.bukkit.entity.Player;
 import world.bentobox.bentobox.api.commands.CompositeCommand;
 import world.bentobox.bentobox.api.user.User;
+import world.bentobox.bentobox.database.objects.Island;
+import world.bentobox.bentobox.managers.IslandsManager;
 
 import java.util.List;
+import java.util.Optional;
 
 public class FlyToggleCommand extends CompositeCommand {
 
@@ -21,6 +24,14 @@ public class FlyToggleCommand extends CompositeCommand {
 
     @Override
     public boolean execute(User user, String label, List<String> args) {
+        // Allow this command only on island
+        final IslandsManager islands = this.getIslands();
+        islands.userIsOnIsland(user.getWorld(), user);
+        final Optional<Island> island = islands.getIslandAt(user.getLocation());
+        if(!island.isPresent() || !island.get().getMembers().containsKey(user.getUniqueId())){
+            user.sendMessage("islandfly.command.only-on-island");
+            return true;
+        }
         final Player player = user.getPlayer();
         if(user.getPlayer().getAllowFlight()){
             // Disable fly and notify player
