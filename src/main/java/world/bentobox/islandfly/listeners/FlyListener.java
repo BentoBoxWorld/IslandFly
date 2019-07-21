4be67941455762e78bd4cb8f72080d403f770142
wellnesscookie <46493763+wellnesscookie@users.noreply.github.com>
2019-07-21 20:40:52 +0200

    Coding style
    4 spaces instead of tab, and so

--- src/main/java/world/bentobox/islandfly/listeners/FlyListener.java
+++ src/main/java/world/bentobox/islandfly/listeners/FlyListener.java
@@ -19,40 +19,49 @@ public class FlyListener implements Listener {
     private final FlySettings settings;
     private final BentoBox plugin;
 
-    public FlyListener(final IslandFlyAddon addon){
+    public FlyListener(final IslandFlyAddon addon) {
+        
         this.settings = addon.getSettings();
         this.plugin = addon.getPlugin();
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onExitIsland(final IslandEvent.IslandExitEvent event){
+    public void onExitIsland(final IslandEvent.IslandExitEvent event) {
+        
         // Check only when player exit is own island
         final UUID playerUUID = event.getPlayerUUID();
+        
         if(!event.getIsland().getMembers().containsKey(playerUUID)) return;
 
         // Player already flying
         final User user = User.getInstance(playerUUID);
         
         if(!user.getPlayer().getAllowFlight()) return;
+        
         // Bypass permission
         if(user.hasPermission(plugin.getIWM().getAddon(user.getWorld()).get().getPermissionPrefix() + "island.flybypass")) return;
 
         // Alert player fly will be disabled
         final int flyTimeout = settings.getFlyTimeout();
         user.sendMessage("islandfly.fly-outside-alert", TextVariables.NUMBER, String.valueOf(flyTimeout));
+        
         // If timeout is 0 or less disable fly immediately
-        if(flyTimeout <= 0){
+        if(flyTimeout <= 0) {
             disableFly(user);
             return;
         }
+        
         // Else disable fly with a delay
-        this.plugin.getServer().getScheduler().runTaskLater(plugin, ()->{
+        this.plugin.getServer().getScheduler().runTaskLater(plugin, ()-> {
+            
             // Verify player is still online
             if(!user.isOnline()) return;
+            
             final IslandsManager islands = plugin.getIslands();
+            
             // Check player is not on his own island
             if(!(islands.userIsOnIsland(user.getWorld(), user)
-            && islands.getIslandAt(user.getLocation()).get().getMembers().containsKey(playerUUID))){
+            && islands.getIslandAt(user.getLocation()).get().getMembers().containsKey(playerUUID))) {
                 disableFly(user);
             }
             else {
@@ -65,8 +74,10 @@ public class FlyListener implements Listener {
      * Disable player fly and alert it
      * @param user
      */
-    private void disableFly(final User user){
+    private void disableFly(final User user) {
+        
         final Player player = user.getPlayer();
+        
         player.setFlying(false);
         player.setAllowFlight(false);
         user.sendMessage("islandfly.disable-fly");
