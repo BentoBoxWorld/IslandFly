7354f1a1d5f5436a1bf23f53079c3682b0875e2f
tastybento <tastybento@wasteofplastic.com>
2019-09-15 16:47:45 -0700

    Register events using BentoBox API to avoid reload issues

--- src/main/java/world/bentobox/islandfly/IslandFlyAddon.java
+++ src/main/java/world/bentobox/islandfly/IslandFlyAddon.java
@@ -1,54 +1,50 @@
 package world.bentobox.islandfly;
 
+import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.islandfly.listeners.FlyDeathListener;
 import world.bentobox.islandfly.listeners.FlyListener;
 import world.bentobox.islandfly.listeners.FlyLogoutListener;
-
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
-import world.bentobox.bentobox.api.addons.Addon;
 
 public class IslandFlyAddon extends Addon {
 
     private FlySettings settings;
     private boolean hooked=false;
-    
-    
+
+
     @Override
     public void onEnable() {
         // Load configuration
         this.settings = new FlySettings(this);
-          
+
         //Hook into gamemodes
         this.getPlugin().getAddonsManager().getGameModeAddons().forEach(gm -> {
             if (gm.getPlayerCommand().isPresent()) {
-        	    new FlyToggleCommand(gm.getPlayerCommand().get());
-        	    hooked=true;
-        	}
-        	
+                new FlyToggleCommand(gm.getPlayerCommand().get());
+                hooked=true;
+            }
+
         });
-        
-        if (hooked) {	
-            
+
+        if (hooked) {
+
             // Register Listeners
-            final PluginManager pluginManager = Bukkit.getPluginManager();
-            pluginManager.registerEvents(new FlyListener(this), this.getPlugin());
-            pluginManager.registerEvents(new FlyDeathListener(this), this.getPlugin());
-            
+            registerListener(new FlyListener(this));
+            registerListener(new FlyDeathListener(this));
+
             // Register listener to disable fly on logout if activated
             if (settings.isFlyDisabledOnLogout()) {
-                pluginManager.registerEvents(new FlyLogoutListener(), this.getPlugin());
+                registerListener(new FlyLogoutListener());
             }
         }
     }
 
-    
+
     @Override
     public void onDisable() {
         //Nothing to do here
     }
 
-    
+
     /**
      * Get addon settings
      * @return settings
