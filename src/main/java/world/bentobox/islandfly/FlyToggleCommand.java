1d1b4e6bec864b0bf98544a444c9eecf807ca97a
tastybento <tastybento@wasteofplastic.com>
2019-11-03 08:24:06 -0800

    Return false if command denied.

--- src/main/java/world/bentobox/islandfly/FlyToggleCommand.java
+++ src/main/java/world/bentobox/islandfly/FlyToggleCommand.java
@@ -34,34 +34,34 @@ public class FlyToggleCommand extends CompositeCommand {
 
     @Override
     public boolean execute(User user, String label, List<String> args) {
-        
-    	// Checks world from corresponding gamemode command with the world player is executing in
-    	if (this.getWorld() != Util.getWorld(user.getWorld())) {
-    	    user.sendMessage("islandfly.wrong-world");
-    	    return true;
-    	}
-    	
-    	// Allow this command only on island
+
+        // Checks world from corresponding gamemode command with the world player is executing in
+        if (this.getWorld() != Util.getWorld(user.getWorld())) {
+            user.sendMessage("islandfly.wrong-world");
+            return false;
+        }
+
+        // Allow this command only on island
         final IslandsManager islands = this.getIslands();
         islands.userIsOnIsland(user.getWorld(), user);
         final Optional<Island> island = islands.getIslandAt(user.getLocation());
-        
+
         if (!user.hasPermission(this.getPermissionPrefix() + "island.flybypass")
-            && (!island.isPresent() || !island.get().getMembers().containsKey(user.getUniqueId()))) {
+                && (!island.isPresent() || !island.get().getMembers().containsKey(user.getUniqueId()))) {
             user.sendMessage("islandfly.command.only-on-island");
-            return true;
+            return false;
         }
-        
+
         final Player player = user.getPlayer();
-	
+
         if (user.getPlayer().getAllowFlight()) {
-        	
+
             // Disable fly and notify player
             player.setFlying(false);
             player.setAllowFlight(false);
             user.sendMessage("islandfly.disable-fly");
         } else {
-        	
+
             // Enable fly and notify player
             player.setAllowFlight(true);
             user.sendMessage("islandfly.enable-fly");
