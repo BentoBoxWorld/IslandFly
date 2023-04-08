33a355065c2a0c25f0b88ca5ce79e605e6ec84de
BONNe <bonne@bonne.id.lv>
2023-04-08 18:55:35 +0300

    Update IslandFlyPladdon.java

--- src/main/java/world/bentobox/islandfly/IslandFlyPladdon.java
+++ src/main/java/world/bentobox/islandfly/IslandFlyPladdon.java
@@ -6,19 +6,11 @@
 
 package world.bentobox.islandfly;
 
-
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 
-/**
- * @author BONNe
- */
-@Plugin(name="IslandFly", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_17)
+
 public class IslandFlyPladdon extends Pladdon
 {
     @Override
